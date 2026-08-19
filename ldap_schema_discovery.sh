195bd664ccf3ae981052987764b10dff50c62060
Elia Pinto <gitter.spiros@gmail.com>
2026-08-19 13:09:54 +0200

    ldap_schema_discovery.sh (Aggiornato con Bind Autenticato)

--- ldap_schema_discovery.sh
+++ ldap_schema_discovery.sh
@@ -1,13 +1,20 @@
-#
-# Taken from https://www.openldap.org/faq/data/cache/1366.html
-#
-#To read the name of the (sub)entry holding the controlling (sub)schema from an entry, say dc=example,dc=com, one could issue the following command:
+#!/bin/bash
+# ==============================================================================
+# SCRIPT:       ldap_schema_discovery.sh (Aggiornato con Bind Autenticato)
+# DESCRIZIONE:  Interroga lo schema OpenLDAP evitando il blocco anonymous bind
+# ==============================================================================
 
-ldapsearch -x -LLL -b dc=example,dc=com -s base subschemaSubentry
-#The value of the subschemaSubentry attribute is the name of the (sub)entry holding the controlling (sub)schema. Note that on current versions of slapd(8), the server supports only a single schema and its always named cn=Subschema, however future versions of slapd(8) might support multiple subschema subentries. Well-behaved clients should not shortcut this procedure.
-#Armed with the name of the (sub)entry holding the (sub)schema, one can then read the desired attributes from this (sub)entry. For instance, one might issue
+# Parametri di connessione (modificabili o personalizzabili via ambiente)
+LDAP_URI="${LDAP_URI:-ldap://localhost}"
+LDAP_BIND_DN="${LDAP_BIND_DN:-cn=admin,dc=example,dc=com}"
+LDAP_PASSWORD="${LDAP_PASSWORD:-openldap}"
+LDAP_BASE="${LDAP_BASE:-dc=example,dc=com}"
 
-ldapsearch -x -LLL -b cn=Subschema -s base '(objectClass=subschema)' attributeTypes dITStructureRules objectClasses nameForms dITContentRules matchingRules ldapSyntaxes matchingRuleUse
-#In servers which supports RFC 3673 you can use a short form:
-ldapsearch -x -LLL -b cn=Subschema -s base '(objectClass=subschema)' +
-#This command will generally produce pages of output, hence it is often appropriate to request, by name, only thos1\e attributes of interest.
+echo "=== 1. Ricerca della subschemaSubentry per: ${LDAP_BASE} ==="
+ldapsearch -x -H "${LDAP_URI}" -D "${LDAP_BIND_DN}" -w "${LDAP_PASSWORD}" \
+  -LLL -b "${LDAP_BASE}" -s base subschemaSubentry
+
+echo -e "\n=== 2. Estrazione dello Schema completo da cn=Subschema ==="
+# Sfrutta la forma breve (RFC 3673) supportata dai server moderni
+ldapsearch -x -H "${LDAP_URI}" -D "${LDAP_BIND_DN}" -w "${LDAP_PASSWORD}" \
+  -LLL -b "cn=Subschema" -s base '(objectClass=subschema)' +
