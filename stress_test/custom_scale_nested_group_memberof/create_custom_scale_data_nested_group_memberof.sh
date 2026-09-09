6d64f62c719d2c95d3238e3bc1b747ff17002693
Elia Pinto <gitter.spiros@gmail.com>
2026-09-09 10:50:02 +0200

    fix invoke

--- stress_test/custom_scale_nested_group_memberof/create_custom_scale_data_nested_group_memberof.sh
+++ stress_test/custom_scale_nested_group_memberof/create_custom_scale_data_nested_group_memberof.sh
@@ -1,5 +1,5 @@
 rm -f create_custom_scale_data_nested_group_memberof.ldif
 unzip create_custom_scale_data_nested_group_memberof.zip
-ansible-playbook ../../restore_master_from_backup.yml --extra-vars 'backup_file=stress_test/custom_scale_nested_group_memberof/create_custom_scale_data_nested_group_memberof.ldif'
+ansible-playbook ../../restore_master_from_backup.yml --extra-vars 'backup_file=stress_test/custom_scale_nested_group_memberof/create_custom_scale_data_nested_group_memberof.ldif' -e confirm_wipe=SI-CONFERMO-IL-RIPRISTINO
 
-#rm create_custom_scale_data_nested_group_memberof.ldif
+rm create_custom_scale_data_nested_group_memberof.ldif
