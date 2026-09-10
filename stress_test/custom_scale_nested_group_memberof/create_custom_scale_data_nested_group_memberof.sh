adf26c65e92ed99a2e6a60b24f92c19338014031
Elia Pinto <gitter.spiros@gmail.com>
2026-09-10 11:47:45 +0200

    new create_custom_scale_data_nested_group_memberof.sh

--- stress_test/custom_scale_nested_group_memberof/create_custom_scale_data_nested_group_memberof.sh
+++ stress_test/custom_scale_nested_group_memberof/create_custom_scale_data_nested_group_memberof.sh
@@ -1,5 +1,6 @@
-rm -f create_custom_scale_data_nested_group_memberof.ldif
-unzip create_custom_scale_data_nested_group_memberof.zip
-ansible-playbook ../../restore_master_from_backup.yml --extra-vars 'backup_file=stress_test/custom_scale_nested_group_memberof/create_custom_scale_data_nested_group_memberof.ldif' -e confirm_wipe=SI-CONFERMO-IL-RIPRISTINO
-
-rm create_custom_scale_data_nested_group_memberof.ldif
+ldif_file="cleanup_custom_scale_data_nested_group_memberof.ldif"
+name_ldif_file="${ldif_file%.*}"
+rm -f $ldif_file
+unzip $name_ldif_file.zip
+ansible-playbook ../../restore_master_from_backup.yml --extra-vars "backup_file=$(pwd)/$ldif_file" -e confirm_wipe=SI-CONFERMO-IL-RIPRISTINO
+rm $ldif_file
