9430c0e7eeedaa1b6981a1617ce95a4c4367474e
Elia Pinto <gitter.spiros@gmail.com>
2026-09-10 11:45:19 +0200

    add new create_custom_scale_data_small.sh and zip file

---
+++ stress_test/small/create_custom_scale_data_small.sh
@@ -0,0 +1,6 @@
+ldif_file="create_custom_scale_data_small.ldif"
+name_ldif_file="${ldif_file%.*}"
+rm -f $ldif_file
+unzip $name_ldif_file.zip
+ansible-playbook ../../restore_master_from_backup.yml --extra-vars "backup_file=$(pwd)/$ldif_file" -e confirm_wipe=SI-CONFERMO-IL-RIPRISTINO
+#rm $ldif_file
