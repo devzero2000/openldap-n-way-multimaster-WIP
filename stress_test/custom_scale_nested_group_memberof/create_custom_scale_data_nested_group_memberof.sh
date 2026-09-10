c7dea03ef59f0679be962934041fa32c1a28faa7
Elia Pinto <gitter.spiros@gmail.com>
2026-09-10 11:57:03 +0200

    litle fix

--- stress_test/custom_scale_nested_group_memberof/create_custom_scale_data_nested_group_memberof.sh
+++ stress_test/custom_scale_nested_group_memberof/create_custom_scale_data_nested_group_memberof.sh
@@ -1,4 +1,4 @@
-ldif_file="cleanup_custom_scale_data_nested_group_memberof.ldif"
+ldif_file="custom_scale_data_nested_group_memberof.ldif"
 name_ldif_file="${ldif_file%.*}"
 rm -f $ldif_file
 unzip $name_ldif_file.zip
