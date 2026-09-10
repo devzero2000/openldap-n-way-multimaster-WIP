80e8a87cdb90c448d34f8eadb39700d91bf78f6a
Elia Pinto <gitter.spiros@gmail.com>
2026-09-10 11:48:40 +0200

    fix create_custom_scale_data_small.sh

--- stress_test/small/create_custom_scale_data_small.sh
+++ stress_test/small/create_custom_scale_data_small.sh
@@ -3,4 +3,4 @@ name_ldif_file="${ldif_file%.*}"
 rm -f $ldif_file
 unzip $name_ldif_file.zip
 ansible-playbook ../../restore_master_from_backup.yml --extra-vars "backup_file=$(pwd)/$ldif_file" -e confirm_wipe=SI-CONFERMO-IL-RIPRISTINO
-#rm $ldif_file
+rm $ldif_file
